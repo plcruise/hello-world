96b952aee3e0399a7693697229cdb976a8c0c8f9
plkrusePTB <47141905+plkrusePTB@users.noreply.github.com>
2020-07-15 14:53:38 +0200

    Tweepy als library hinzugefuegt. erster Tweet erfolgreich von ausserhlab des PTB netzes

--- PyTweeting.py
+++ PyTweeting.py
@@ -1,4 +1,9 @@
-def bra(): 
-    print("der bra")
 
-    
+import tweepy
+auth = tweepy.OAuthHandler('DJ3yOjQhddvAMkhDffs85kmfR','yIw8OWpdoAOL167xpRBgpRtkBgHA7CuQQlI3M5DD2CFBNBnkuk')
+
+auth.set_access_token('1166597179135471621-CtRH6kHoxYecyKlI0xOI7PeGV9lHAR','XaCGjngj88Ed6AZEnYh22mZLrFGCAI1RSaBV6XftO3rQG')
+
+api = tweepy.API(auth)
+
+api.update_status("Tweepy automated Tweet.")
