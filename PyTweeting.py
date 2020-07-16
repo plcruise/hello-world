6cb69fc261460b9aa5b7303fe771d4941d792dbd
plkrusePTB <47141905+plkrusePTB@users.noreply.github.com>
2020-07-16 07:16:54 +0200

    Added a Python Container Configuration and commands to install virtual environment

--- PyTweeting.py
+++ PyTweeting.py
@@ -4,6 +4,6 @@ auth = tweepy.OAuthHandler('DJ3yOjQhddvAMkhDffs85kmfR','yIw8OWpdoAOL167xpRBgpRtk
 
 auth.set_access_token('1166597179135471621-CtRH6kHoxYecyKlI0xOI7PeGV9lHAR','XaCGjngj88Ed6AZEnYh22mZLrFGCAI1RSaBV6XftO3rQG')
 
-api = tweepy.API(auth)
+api = tweepy.API(auth, proxy='https://webproxy.bs.ptb.de:8080')
 
-api.update_status("Tweepy automated Tweet.")
+api.update_status("Tweepy automated1 Tweet.")
