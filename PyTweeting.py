6ef468e0be3b9a58a2a153b7aa6e65c8986eae25
plkrusePTB <47141905+plkrusePTB@users.noreply.github.com>
2020-07-16 12:28:08 +0200

    added Random String generator bash command for execution of PyTweeting.ps

--- PyTweeting.py
+++ PyTweeting.py
@@ -1,9 +1,19 @@
 
 import tweepy
+import random
+import string
+
+
+def generate_random_string(): 
+    letters = string.ascii_lowercase
+    res_string = ''.join(random.choice(letters) for i in range(8) )
+    return res_string
+
+print(generate_random_string())
 auth = tweepy.OAuthHandler('DJ3yOjQhddvAMkhDffs85kmfR','yIw8OWpdoAOL167xpRBgpRtkBgHA7CuQQlI3M5DD2CFBNBnkuk')
 
 auth.set_access_token('1166597179135471621-CtRH6kHoxYecyKlI0xOI7PeGV9lHAR','XaCGjngj88Ed6AZEnYh22mZLrFGCAI1RSaBV6XftO3rQG')
 
 api = tweepy.API(auth, proxy='https://webproxy.bs.ptb.de:8080')
 
-api.update_status("Tweepy automated1 Tweet.")
+api.update_status(generate_random_string())
